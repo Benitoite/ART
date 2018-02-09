Benitoite/ART

commit 49ae7f0b8e8b803b3f0bda86c7f0f8b75efe57ae

feature: added option to use a (fast) neutral RAW rendering in 'inspector mode'

rtengine/rtthumbnail.cc
@@ -36,7 +36,9 @@
 #include "jpeg.h"
 #include "../rtgui/ppversion.h"
 #include "improccoordinator.h"
+#include "settings.h"
 #include <locale.h>
+#include "StopWatch.h"
 
 namespace
 {
@@ -146,6 +148,8 @@ extern Options options;
 
 namespace rtengine
 {
+
+extern const Settings *settings;
 
 using namespace procparams;
 
@@ -261,13 +265,83 @@ Thumbnail* Thumbnail::loadFromImage (const Glib::ustring& fname, int &w, int &h,
     return tpp;
 }
 
+
+namespace {
+
+Image8 *load_inspector_mode(const Glib::ustring &fname, RawMetaDataLocation &rml, eSensorType &sensorType, int &w, int &h)
+{
+    BENCHFUN
+    
+    RawImageSource src;
+    int err = src.load(fname, true);
+    if (err) {
+        return nullptr;
+    }
+
+    src.getFullSize(w, h);
+    sensorType = src.getSensorType();
+    
+    ProcParams neutral;
+    neutral.raw.bayersensor.method = RAWParams::BayerSensor::getMethodString(RAWParams::BayerSensor::Method::FAST);
+    neutral.raw.xtranssensor.method = RAWParams::XTransSensor::getMethodString(RAWParams::XTransSensor::Method::FAST);
+    neutral.icm.input = "(camera)";
+    neutral.icm.working = "RT_sRGB";
+
+    src.preprocess(neutral.raw, neutral.lensProf, neutral.coarse, false);
+    src.demosaic(neutral.raw);
+
+    PreviewProps pp(0, 0, w, h, 1);
+
+    Imagefloat tmp(w, h);
+    src.getImage(src.getWB(), TR_NONE, &tmp, pp, neutral.toneCurve, neutral.raw);
+    src.convertColorSpace(&tmp, neutral.icm, src.getWB());
+
+    Image8 *img = new Image8(w, h);
+    const float f = 255.f/65535.f;
+#ifdef _OPENMP
+    #pragma omp parallel for
+#endif
+    for (int y = 0; y < h; ++y) {
+        for (int x = 0; x < w; ++x) {
+            img->r(y, x) = Color::gamma_srgbclipped(tmp.r(y, x)) * f;
+            img->g(y, x) = Color::gamma_srgbclipped(tmp.g(y, x)) * f;
+            img->b(y, x) = Color::gamma_srgbclipped(tmp.b(y, x)) * f;
+        }
+    }
+
+    return img;
+}
+
+} // namespace
+
 Thumbnail* Thumbnail::loadQuickFromRaw (const Glib::ustring& fname, RawMetaDataLocation& rml, eSensorType &sensorType, int &w, int &h, int fixwh, bool rotate, bool inspectorMode)
 {
+    Thumbnail* tpp = new Thumbnail ();
+    tpp->isRaw = 1;
+    memset (tpp->colorMatrix, 0, sizeof (tpp->colorMatrix));
+    tpp->colorMatrix[0][0] = 1.0;
+    tpp->colorMatrix[1][1] = 1.0;
+    tpp->colorMatrix[2][2] = 1.0;
+
+    if (inspectorMode && settings->thumbnail_inspector_raw) {
+        Image8 *img = load_inspector_mode(fname, rml, sensorType, w, h);
+        if (!img) {
+            delete tpp;
+            return nullptr;
+        }
+
+        tpp->scale = 1.;
+        tpp->thumbImg = img;
+
+        return tpp;
+    }
+    
     RawImage *ri = new RawImage (fname);
     unsigned int imageNum = 0;
     int r = ri->loadRaw (false, imageNum, false);
 
     if ( r ) {
+        delete tpp;
         delete ri;
         sensorType = ST_NONE;
         return nullptr;
@@ -301,18 +375,11 @@ Thumbnail* Thumbnail::loadQuickFromRaw (const Glib::ustring& fname, RawMetaDataL
     // did we succeed?
     if ( err ) {
         printf ("Could not extract thumb from %s\n", fname.data());
+        delete tpp;
         delete img;
         delete ri;
         return nullptr;
     }
-
-    Thumbnail* tpp = new Thumbnail ();
-
-    tpp->isRaw = 1;
-    memset (tpp->colorMatrix, 0, sizeof (tpp->colorMatrix));
-    tpp->colorMatrix[0][0] = 1.0;
-    tpp->colorMatrix[1][1] = 1.0;
-    tpp->colorMatrix[2][2] = 1.0;
 
     if (inspectorMode) {
         // Special case, meaning that we want a full sized thumbnail image (e.g. for the Inspector feature)

rtdata/languages/default
@@ -1154,6 +1154,9 @@ PREFERENCES_TAB_IMPROC;Image Processing
 PREFERENCES_TAB_PERFORMANCE;Performance & Quality
 PREFERENCES_TAB_SOUND;Sounds
 PREFERENCES_THEME;Theme
+PREFERENCES_THUMBNAIL_INSPECTOR_JPEG;Embedded JPEG preview
+PREFERENCES_THUMBNAIL_INSPECTOR_MODE;Image to show
+PREFERENCES_THUMBNAIL_INSPECTOR_RAW;Neutral RAW rendering 
 PREFERENCES_TIMAX;High
 PREFERENCES_TINB;Number of tiles
 PREFERENCES_TISTD;Standard

rtengine/rawimagesource.cc
@@ -1517,7 +1517,7 @@ void RawImageSource::vflip (Imagefloat* image)
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-int RawImageSource::load (const Glib::ustring &fname)
+int RawImageSource::load (const Glib::ustring &fname, bool firstFrameOnly)
 {
 
     MyTime t1, t2;
@@ -1534,7 +1534,7 @@ int RawImageSource::load (const Glib::ustring &fname)
     if (errCode) {
         return errCode;
     }
-    numFrames = ri->getFrameCount();
+    numFrames = firstFrameOnly ? 1 : ri->getFrameCount();
 
     errCode = 0;
 

rtengine/rawimagesource.h
@@ -118,7 +118,8 @@ public:
     RawImageSource ();
     ~RawImageSource ();
 
-    int         load        (const Glib::ustring &fname);
+    int load(const Glib::ustring &fname) { return load(fname, false); }
+    int load(const Glib::ustring &fname, bool firstFrameOnly);
     void        preprocess  (const RAWParams &raw, const LensProfParams &lensProf, const CoarseTransformParams& coarse, bool prepareDenoise = true);
     void        demosaic    (const RAWParams &raw);
     void        retinex       (const ColorManagementParams& cmp, const RetinexParams &deh, const ToneCurveParams& Tc, LUTf & cdcurve, LUTf & mapcurve, const RetinextransmissionCurve & dehatransmissionCurve, const RetinexgaintransmissionCurve & dehagaintransmissionCurve, multi_array2D<float, 4> &conversionBuffer, bool dehacontlutili, bool mapcontlutili, bool useHsl, float &minCD, float &maxCD, float &mini, float &maxi, float &Tmean, float &Tsigma, float &Tmin, float &Tmax, LUTu &histLRETI);

rtengine/settings.h
@@ -84,6 +84,8 @@ public:
     double          level0_cbdl;
     double          level123_cbdl;
     Glib::ustring   lensfunDbDirectory; ///< The directory containing the lensfun database. If empty, the system defaults will be used (as described in http://lensfun.sourceforge.net/manual/dbsearch.html)
+
+    bool thumbnail_inspector_raw;
     
     /** Creates a new instance of Settings.
       * @return a pointer to the new Settings instance. */

rtgui/options.cc
@@ -606,6 +606,8 @@ void Options::setDefaults ()
     rtSettings.lensfunDbDirectory = ""; // set also in main.cc and main-cli.cc
     cropGuides = CROP_GUIDE_FULL;
     cropAutoFit = false;
+
+    rtSettings.thumbnail_inspector_raw = false;
 }
 
 Options* Options::copyFrom (Options* other)
@@ -1078,6 +1080,10 @@ void Options::readFromFile (Glib::ustring fname)
 
                 if (keyFile.has_key ("Performance", "SerializeTiffRead")) {
                     serializeTiffRead = keyFile.get_boolean ("Performance", "SerializeTiffRead");
+                }
+
+                if (keyFile.has_key("Performance", "ThumbnailInspectorRaw")) {
+                    rtSettings.thumbnail_inspector_raw = keyFile.get_boolean("Performance", "ThumbnailInspectorRaw");
                 }
             }
 
@@ -1861,6 +1867,7 @@ void Options::saveToFile (Glib::ustring fname)
         keyFile.set_integer ("Performance", "PreviewDemosaicFromSidecar", prevdemo);
         keyFile.set_boolean ("Performance", "Daubechies", rtSettings.daubech);
         keyFile.set_boolean ("Performance", "SerializeTiffRead", serializeTiffRead);
+        keyFile.set_boolean("Performance", "ThumbnailInspectorRaw", rtSettings.thumbnail_inspector_raw);
 
         keyFile.set_string  ("Output", "Format", saveFormat.format);
         keyFile.set_integer ("Output", "JpegQuality", saveFormat.jpegQuality);

rtgui/preferences.cc
@@ -657,7 +657,18 @@ Gtk::Widget* Preferences::getPerformancePanel ()
     maxInspectorBuffersSB->set_range (1, 12); // ... we have to set a limit, 12 seem to be enough even for systems with tons of RAM
     maxIBuffersHB->pack_start (*maxIBufferLbl, Gtk::PACK_SHRINK, 0);
     maxIBuffersHB->pack_end (*maxInspectorBuffersSB, Gtk::PACK_SHRINK, 0);
-    finspect->add (*maxIBuffersHB);
+
+    Gtk::VBox *inspectorvb = Gtk::manage(new Gtk::VBox());
+    inspectorvb->add(*maxIBuffersHB);
+
+    Gtk::HBox *insphb = Gtk::manage(new Gtk::HBox());
+    thumbnailInspectorMode = Gtk::manage(new Gtk::ComboBoxText());
+    thumbnailInspectorMode->append(M("PREFERENCES_THUMBNAIL_INSPECTOR_JPEG"));
+    thumbnailInspectorMode->append(M("PREFERENCES_THUMBNAIL_INSPECTOR_RAW"));
+    insphb->pack_start(*Gtk::manage(new Gtk::Label(M("PREFERENCES_THUMBNAIL_INSPECTOR_MODE") + ": ")), Gtk::PACK_SHRINK, 4);
+    insphb->pack_start(*thumbnailInspectorMode);
+    inspectorvb->pack_start(*insphb);
+    finspect->add (*inspectorvb);
     mainContainer->pack_start (*finspect, Gtk::PACK_SHRINK, 4);
 
     Gtk::Frame* fdenoise = Gtk::manage ( new Gtk::Frame (M ("PREFERENCES_NOISE")) );
@@ -1853,6 +1864,7 @@ void Preferences::storePreferences ()
     moptions.rgbDenoiseThreadLimit = rgbDenoiseTreadLimitSB->get_value_as_int();
     moptions.clutCacheSize = clutCacheSizeSB->get_value_as_int();
     moptions.maxInspectorBuffers = maxInspectorBuffersSB->get_value_as_int();
+    moptions.rtSettings.thumbnail_inspector_raw = thumbnailInspectorMode->get_active_row_number() == 1;
 
 // Sounds only on Windows and Linux
 #if defined(WIN32) || defined(__linux__)
@@ -2072,6 +2084,7 @@ void Preferences::fillPreferences ()
     rgbDenoiseTreadLimitSB->set_value (moptions.rgbDenoiseThreadLimit);
     clutCacheSizeSB->set_value (moptions.clutCacheSize);
     maxInspectorBuffersSB->set_value (moptions.maxInspectorBuffers);
+    thumbnailInspectorMode->set_active(moptions.rtSettings.thumbnail_inspector_raw ? 1 : 0);
 
     darkFrameDir->set_current_folder ( moptions.rtSettings.darkFramesPath );
     darkFrameChanged ();

rtgui/preferences.h
@@ -166,6 +166,7 @@ class Preferences : public Gtk::Dialog, public ProfileStoreListener
     Gtk::SpinButton*  rgbDenoiseTreadLimitSB;
     Gtk::SpinButton*  clutCacheSizeSB;
     Gtk::SpinButton*  maxInspectorBuffersSB;
+    Gtk::ComboBoxText *thumbnailInspectorMode;
 
     Gtk::CheckButton* ckbmenuGroupRank;
     Gtk::CheckButton* ckbmenuGroupLabel;

commit db079abf63d65727102ebf57ed16cc5c08615c10

fixed regression in histogram matching caused by 49ae7f0b8e8b803b3f0bda86c7f0f8b75efe57ae

rtengine/histmatching.cc
@@ -219,7 +219,7 @@ void RawImageSource::getAutoMatchedToneCurve(const ColorManagementParams &cp, st
         RawMetaDataLocation rml;
         eSensorType sensor_type;
         int w, h;
-        std::unique_ptr<Thumbnail> thumb(Thumbnail::loadQuickFromRaw(getFileName(), rml, sensor_type, w, h, 1, false, true));
+        std::unique_ptr<Thumbnail> thumb(Thumbnail::loadQuickFromRaw(getFileName(), rml, sensor_type, w, h, 1, false, true, true));
         if (!thumb) {
             if (settings->verbose) {
                 std::cout << "histogram matching: no thumbnail found, generating a neutral curve" << std::endl;

rtengine/rtthumbnail.cc
@@ -314,7 +314,7 @@ Image8 *load_inspector_mode(const Glib::ustring &fname, RawMetaDataLocation &rml
 
 } // namespace
 
-Thumbnail* Thumbnail::loadQuickFromRaw (const Glib::ustring& fname, RawMetaDataLocation& rml, eSensorType &sensorType, int &w, int &h, int fixwh, bool rotate, bool inspectorMode)
+Thumbnail* Thumbnail::loadQuickFromRaw (const Glib::ustring& fname, RawMetaDataLocation& rml, eSensorType &sensorType, int &w, int &h, int fixwh, bool rotate, bool inspectorMode, bool forHistogramMatching)
 {
     Thumbnail* tpp = new Thumbnail ();
     tpp->isRaw = 1;
@@ -323,7 +323,7 @@ Thumbnail* Thumbnail::loadQuickFromRaw (const Glib::ustring& fname, RawMetaDataL
     tpp->colorMatrix[1][1] = 1.0;
     tpp->colorMatrix[2][2] = 1.0;
 
-    if (inspectorMode && settings->thumbnail_inspector_raw) {
+    if (inspectorMode && !forHistogramMatching && settings->thumbnail_inspector_raw) {
         Image8 *img = load_inspector_mode(fname, rml, sensorType, w, h);
         if (!img) {
             delete tpp;

rtengine/rtthumbnail.h
@@ -76,7 +76,7 @@ public:
     int      getImageWidth  (const procparams::ProcParams& pparams, int rheight, float &ratio);
     void     getDimensions  (int& w, int& h, double& scaleFac);
 
-    static Thumbnail* loadQuickFromRaw (const Glib::ustring& fname, rtengine::RawMetaDataLocation& rml, eSensorType &sensorType, int &w, int &h, int fixwh, bool rotate, bool inspectorMode = false);
+    static Thumbnail* loadQuickFromRaw (const Glib::ustring& fname, rtengine::RawMetaDataLocation& rml, eSensorType &sensorType, int &w, int &h, int fixwh, bool rotate, bool inspectorMode = false, bool forHistogramMatching = false);
     static Thumbnail* loadFromRaw (const Glib::ustring& fname, RawMetaDataLocation& rml, eSensorType &sensorType, int &w, int &h, int fixwh, double wbEq, bool rotate, bool forHistogramMatching = false);
     static Thumbnail* loadFromImage (const Glib::ustring& fname, int &w, int &h, int fixwh, double wbEq, bool inspectorMode = false);
     static RawMetaDataLocation loadMetaDataFromRaw (const Glib::ustring& fname);