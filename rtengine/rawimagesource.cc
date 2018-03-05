d024ec45e712389ca88e268313bd12e275d244e8
Alberto Griggio <agriggio@users.noreply.github.com>
2018-03-05 17:46:20 +0100

    fixed missing update of the total number of bad pixels found

--- rtengine/rawimagesource.cc
+++ rtengine/rawimagesource.cc
@@ -1873,6 +1873,7 @@ void RawImageSource::preprocess  (const RAWParams &raw, const LensProfParams &le
         }
         
         int n = f.mark(rawData, *bitmapBads);
+        totBP += n;
 
         if (settings->verbose && n > 0) {
             printf("Marked %d hot pixels from PDAF lines\n", n);            
