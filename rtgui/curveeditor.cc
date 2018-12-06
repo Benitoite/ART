bd67fe86b7b334d1909841f0e62b8e5307c14ab2
Alberto Griggio <agriggio@users.noreply.github.com>
2018-12-06 14:09:07 +0100

    added custom icon for catmull-rom curves

--- rtgui/curveeditor.cc
+++ rtgui/curveeditor.cc
@@ -76,7 +76,7 @@ DiagonalCurveEditor::DiagonalCurveEditor (Glib::ustring text, CurveEditorGroup* 
 
     curveType->addEntry("curve-linear-small.png", M("CURVEEDITOR_LINEAR")); // 0 Linear
     curveType->addEntry("curve-spline-small.png", M("CURVEEDITOR_CUSTOM")); // 1 Spline
-    curveType->addEntry("curve-spline-small.png", M("CURVEEDITOR_CATMULLROM")); // 4 CatmullRom
+    curveType->addEntry("curve-catmullrom-small.png", M("CURVEEDITOR_CATMULLROM")); // 4 CatmullRom
     curveType->addEntry("curve-parametric-small.png", M("CURVEEDITOR_PARAMETRIC")); // 2 Parametric
     curveType->addEntry("curve-nurbs-small.png", M("CURVEEDITOR_NURBS")); // 3 NURBS
     static_cast<CurveTypePopUpButton *>(curveType)->setPosIndexMap({ 0, 1, 4, 2, 3 });
