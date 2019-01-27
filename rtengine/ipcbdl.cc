0676b9519bc5990b3469882813e149516f09886d
Alberto Griggio <agriggio@users.noreply.github.com>
2019-01-27 18:57:51 +0100

    cbdl: improved params adjustments with scale

--- rtengine/ipcbdl.cc
+++ rtengine/ipcbdl.cc
@@ -55,9 +55,16 @@ void ImProcFunctions::contrastByDetailLevels(LabImage* lab, int offset_x, int of
 
         array2D<float> L(lab->W, lab->H, lab->L, 0);
 
+        double mult[6];
+        const double scale_factor = min(1.5 / scale, 1.0);
+
         for (int i = 0; i < n; ++i) {
             auto &l = params->dirpyrequalizer.levels[i];
-            dirpyr_equalizer(lab->L, L, lab->W, lab->H, lab->a, lab->b, l.mult, l.threshold, 0.0, 0.f, 0.f, 0.f, std::max(scale, 1.0));
+            for (int k = 0; k < 6; ++k) {
+                mult[k] = 1.0 + (l.mult[k] - 1.0) * scale_factor;
+            }
+            const double threshold = l.threshold / scale;
+            dirpyr_equalizer(lab->L, L, lab->W, lab->H, lab->a, lab->b, /*l.*/mult, /*l.*/threshold, 0.0, 0.f, 0.f, 0.f, 1.0);//std::max(scale, 1.0));
             const auto &blend = mask[i];
 
 #ifdef _OPENMP
