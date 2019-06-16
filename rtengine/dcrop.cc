046b31e5404c88ee904cd80d295fda57306901f5
Alberto Griggio <agriggio@users.noreply.github.com>
2019-06-16 23:28:42 +0200

    use a larger crop if perspective correction is enabled

--- rtengine/dcrop.cc
+++ rtengine/dcrop.cc
@@ -438,16 +438,15 @@ void Crop::freeAll()
 }
 
 
-namespace
-{
+namespace {
 
-bool check_need_larger_crop_for_lcp_distortion(int fw, int fh, int x, int y, int w, int h, const ProcParams &params)
+bool check_need_larger_crop_for_transform(int fw, int fh, int x, int y, int w, int h, const ProcParams &params)
 {
     if (x == 0 && y == 0 && w == fw && h == fh) {
         return false;
     }
 
-    return (params.lensProf.useDist && (params.lensProf.useLensfun() || params.lensProf.useLcp()));
+    return (params.lensProf.useDist && (params.lensProf.useLensfun() || params.lensProf.useLcp())) || params.perspective.enabled;
 }
 
 } // namespace
@@ -502,7 +501,7 @@ bool Crop::setCropSizes(int rcx, int rcy, int rcw, int rch, int skip, bool inter
 
     parent->ipf.transCoord(parent->fw, parent->fh, bx1, by1, bw, bh, orx, ory, orw, orh);
 
-    if (check_need_larger_crop_for_lcp_distortion(parent->fw, parent->fh, orx, ory, orw, orh, parent->params)) {
+    if (check_need_larger_crop_for_transform(parent->fw, parent->fh, orx, ory, orw, orh, parent->params)) {
         // TODO - this is an estimate of the max distortion relative to the image size. ATM it is hardcoded to be 15%, which seems enough. If not, need to revise
         int dW = int (double (parent->fw) * 0.15 / (2 * skip));
         int dH = int (double (parent->fh) * 0.15 / (2 * skip));
