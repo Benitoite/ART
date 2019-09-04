303e0d48e16d5f98f56e616d5ca76784ccaee70d
Hombre <natureh.510@gmail.com>
2019-09-05 01:09:48 +0200

    The svg2png icon cache is now cleared on new minor version
    The `version` value from the `Options` file is compared to the harcoded
    value (computed at build time) of the running application. If the major
    or minor version is different, the `svg2png` folder is cleaned at
    startup-time.
    
    (Ported from RT)

--- rtgui/rtimage.cc
+++ rtgui/rtimage.cc
@@ -173,7 +173,7 @@ void RTImage::init()
     scaleBack = RTScalable::getScale();
 }
 
-void RTImage::cleanup()
+void RTImage::cleanup(bool all)
 {
     for (auto& entry : pixbufCache) {
         entry.second.reset();
@@ -181,7 +181,7 @@ void RTImage::cleanup()
     for (auto& entry : surfaceCache) {
         entry.second.clear();
     }
-    RTScalable::cleanup();
+    RTScalable::cleanup(all);
 }
 
 void RTImage::updateImages()
