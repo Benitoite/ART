7eafc72ad453c14d3b10f23e87cf5a7d1c7fbf9e
Alberto Griggio <agriggio@users.noreply.github.com>
2019-09-06 10:07:43 +0200

    allow UpdateInfo to work also on mercurial repos (with hg-git)

--- UpdateInfo.cmake
+++ UpdateInfo.cmake
@@ -3,51 +3,110 @@
 # If we find ReleaseInfo.cmake we use the info from there and don't need Git to be installed
 find_file(REL_INFO_FILE ReleaseInfo.cmake PATHS "${PROJECT_SOURCE_DIR}" NO_DEFAULT_PATH)
 if(REL_INFO_FILE STREQUAL REL_INFO_FILE-NOTFOUND)
-    # we look for the git command in this paths by order of preference
-    if(WIN32)
-        find_program(GIT_CMD git.exe HINTS ENV Path PATH_SUFFIXES ../)
-    elseif(APPLE)
-        find_program(GIT_CMD git PATHS "/opt/local/bin" "/usr/local/bin" "/usr/bin")
-        find_program(GIT_CMD git)
-        set(SHELL "/bin/bash")
-    else(WIN32) # Linux
-        find_program(GIT_CMD git)
-        set(SHELL "/bin/bash")
-    endif(WIN32)
+    if(EXISTS "${PROJECT_SOURCE_DIR}/.git")
+        # this is a git repo
+        
+        # we look for the git command in this paths by order of preference
+        if(WIN32)
+            find_program(GIT_CMD git.exe HINTS ENV Path PATH_SUFFIXES ../)
+        elseif(APPLE)
+            find_program(GIT_CMD git PATHS "/opt/local/bin" "/usr/local/bin" "/usr/bin")
+            find_program(GIT_CMD git)
+            set(SHELL "/bin/bash")
+        else() # Linux
+            find_program(GIT_CMD git)
+            set(SHELL "/bin/bash")
+        endif()
 
-    # Fail if Git is not installed
-    if(GIT_CMD STREQUAL GIT_CMD-NOTFOUND)
-        message(FATAL_ERROR "git command not found!")
+        # Fail if Git is not installed
+        if(GIT_CMD STREQUAL GIT_CMD-NOTFOUND)
+            message(FATAL_ERROR "git command not found!")
+        else()
+            message(STATUS "git command found: ${GIT_CMD}")
+        endif()
+
+        # Get version description.
+        # Depending on whether you checked out a branch (dev) or a tag (release),
+        # "git describe" will return "5.0-gtk2-2-g12345678" or "5.0-gtk2", respectively.
+        execute_process(COMMAND ${GIT_CMD} describe --tags --always OUTPUT_VARIABLE GIT_DESCRIBE OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        # Get branch name.
+        # Will return empty if you checked out a commit or tag. Empty string handled later.
+        execute_process(COMMAND ${GIT_CMD} symbolic-ref --short -q HEAD OUTPUT_VARIABLE GIT_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        # Get commit hash.
+        execute_process(COMMAND ${GIT_CMD} rev-parse --short --verify HEAD OUTPUT_VARIABLE GIT_COMMIT OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        # Get commit date, YYYY-MM-DD.
+        execute_process(COMMAND ${GIT_CMD} show -s --format=%cd --date=format:%Y-%m-%d OUTPUT_VARIABLE GIT_COMMIT_DATE OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        # Get number of commits since tagging. This is what "GIT_DESCRIBE" uses.
+        # Works when checking out branch, tag or commit.
+        # Get a list of all tags in repo:
+        execute_process(COMMAND ${GIT_CMD} tag --merged HEAD OUTPUT_VARIABLE GIT_TAG WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+        # Replace newlines with semicolons so that it can be split:
+        string(REPLACE "\n" ";" GIT_TAG_LIST "${GIT_TAG}")
+        execute_process(COMMAND ${GIT_CMD} rev-list --count HEAD --not ${GIT_TAG_LIST} OUTPUT_VARIABLE GIT_COMMITS_SINCE_TAG OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        # Get number of commits since branching.
+        # Works when checking out branch, tag or commit.
+        execute_process(COMMAND ${GIT_CMD} rev-list --count HEAD --not --tags OUTPUT_VARIABLE GIT_COMMITS_SINCE_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+    elseif(EXISTS "${PROJECT_SOURCE_DIR}/.hg")
+        # a hg-git repo (i.e. Alberto's one :-)
+
+        # we look for the hg command in this paths by order of preference
+        if(WIN32)
+            find_program(HG_CMD hg.exe HINTS ENV Path PATH_SUFFIXES ../)
+        elseif(APPLE)
+            find_program(HG_CMD hg PATHS "/opt/local/bin" "/usr/local/bin" "/usr/bin")
+            find_program(HG_CMD hg)
+            set(SHELL "/bin/bash")
+        else() # Linux
+            find_program(HG_CMD hg)
+            set(SHELL "/bin/bash")
+        endif()
+
+        # Fail if Mercurial is not installed
+        if(HG_CMD STREQUAL HG_CMD-NOTFOUND)
+            message(FATAL_ERROR "hg command not found!")
+        else()
+            message(STATUS "hg command found: ${HG_CMD}")
+        endif()
+        
+        # we emulate the behaviour of git with Mercurial
+        execute_process(COMMAND ${HG_CMD} log -r .
+            --template "{sub('^.*/.*:', '', latesttag)}{ifeq(latesttagdistance, 0, '', '-')}{ifeq(latesttagdistance, 0, '', latesttagdistance)}{ifeq(latesttagdistance, 0, '', '-g')}{ifeq(latesttagdistance, 0, '', sub('.....$', '', short(gitnode)))}"
+            OUTPUT_VARIABLE GIT_DESCRIBE
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        execute_process(COMMAND ${HG_CMD} log -r . --template "{activebookmark}"
+            OUTPUT_VARIABLE GIT_BRANCH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        execute_process(COMMAND ${HG_CMD} log -r . --template "{sub('.....$', '', short(gitnode))}"
+            OUTPUT_VARIABLE GIT_COMMIT
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        execute_process(COMMAND ${HG_CMD} log -r . --template "{date|shortdate}"
+            OUTPUT_VARIABLE GIT_COMMIT_DATE
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        execute_process(COMMAND ${HG_CMD} log -r . --template "{latesttagdistance}"
+            OUTPUT_VARIABLE GIT_COMMITS_SINCE_TAG
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+
+        execute_process(COMMAND ${HG_CMD} log -r . --template "{count(revset('.::bookmark()'))-1}"
+            OUTPUT_VARIABLE GIT_COMMITS_SINCE_BRANCH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
     else()
-        message(STATUS "git command found: ${GIT_CMD}")
+        message(FATAL_ERROR "not inside a repository!")
     endif()
-
-    # Get version description.
-    # Depending on whether you checked out a branch (dev) or a tag (release),
-    # "git describe" will return "5.0-gtk2-2-g12345678" or "5.0-gtk2", respectively.
-    execute_process(COMMAND ${GIT_CMD} describe --tags --always OUTPUT_VARIABLE GIT_DESCRIBE OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-
-    # Get branch name.
-    # Will return empty if you checked out a commit or tag. Empty string handled later.
-    execute_process(COMMAND ${GIT_CMD} symbolic-ref --short -q HEAD OUTPUT_VARIABLE GIT_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-
-    # Get commit hash.
-    execute_process(COMMAND ${GIT_CMD} rev-parse --short --verify HEAD OUTPUT_VARIABLE GIT_COMMIT OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-
-    # Get commit date, YYYY-MM-DD.
-    execute_process(COMMAND ${GIT_CMD} show -s --format=%cd --date=format:%Y-%m-%d OUTPUT_VARIABLE GIT_COMMIT_DATE OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-
-    # Get number of commits since tagging. This is what "GIT_DESCRIBE" uses.
-    # Works when checking out branch, tag or commit.
-    # Get a list of all tags in repo:
-    execute_process(COMMAND ${GIT_CMD} tag --merged HEAD OUTPUT_VARIABLE GIT_TAG WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-    # Replace newlines with semicolons so that it can be split:
-    string(REPLACE "\n" ";" GIT_TAG_LIST "${GIT_TAG}")
-    execute_process(COMMAND ${GIT_CMD} rev-list --count HEAD --not ${GIT_TAG_LIST} OUTPUT_VARIABLE GIT_COMMITS_SINCE_TAG OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-
-    # Get number of commits since branching.
-    # Works when checking out branch, tag or commit.
-    execute_process(COMMAND ${GIT_CMD} rev-list --count HEAD --not --tags OUTPUT_VARIABLE GIT_COMMITS_SINCE_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
 
     # If user checked-out something which is not a branch, use the description as branch name.
     if(GIT_BRANCH STREQUAL "")
@@ -80,9 +139,9 @@ if(REL_INFO_FILE STREQUAL REL_INFO_FILE-NOTFOUND)
         message(STATUS "CACHE_NAME_SUFFIX is \"${CACHE_NAME_SUFFIX}\"")
     endif()
 
-else(REL_INFO_FILE STREQUAL REL_INFO_FILE-NOTFOUND)
+else()
     include("${PROJECT_SOURCE_DIR}/ReleaseInfo.cmake")
-endif(REL_INFO_FILE STREQUAL REL_INFO_FILE-NOTFOUND)
+endif()
 
 if(WIN32)
     if(BIT_DEPTH EQUAL 4)
