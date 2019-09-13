Benitoite/ART

commit dc5342345f15b39e249568e85da0f9c9ebc19ab3

removed useless toolbars from the file panel

rtgui/filecatalog.cc
@@ -42,7 +42,7 @@ using namespace std;
 
 #define CHECKTIME 2000
 
-FileCatalog::FileCatalog (CoarsePanel* cp, ToolBar* tb, FilePanel* filepanel) :
+FileCatalog::FileCatalog(FilePanel* filepanel) :
     filepanel(filepanel),
     selectedDirectoryId(1),
     actionNextPrevious(NAV_NONE),
@@ -52,12 +52,9 @@ FileCatalog::FileCatalog (CoarsePanel* cp, ToolBar* tb, FilePanel* filepanel) :
     hbToolBar1STB(nullptr),
     hasValidCurrentEFS(false),
     filterPanel(nullptr),
-//    exportPanel(nullptr),
     previewsToLoad(0),
     previewsLoaded(0),
-    modifierKey(0),
-    coarsePanel(cp),
-    toolBar(tb)
+    modifierKey(0)
 {
 
     inTabMode = false;
@@ -435,11 +432,6 @@ FileCatalog::FileCatalog (CoarsePanel* cp, ToolBar* tb, FilePanel* filepanel) :
     tbRightPanel_1->set_image (*iRightPanel_1_Hide);
     tbRightPanel_1->signal_toggled().connect( sigc::mem_fun(*this, &FileCatalog::tbRightPanel_1_toggled) );
     buttonBar->pack_end (*tbRightPanel_1, Gtk::PACK_SHRINK);
-
-    buttonBar->pack_end (*coarsePanel, Gtk::PACK_SHRINK);
-    buttonBar->pack_end (*Gtk::manage(new Gtk::VSeparator), Gtk::PACK_SHRINK, 4);
-    buttonBar->pack_end (*toolBar, Gtk::PACK_SHRINK);
-    buttonBar->pack_end (*Gtk::manage(new Gtk::VSeparator), Gtk::PACK_SHRINK, 4);
 
     // add default panel
     hBox = Gtk::manage( new Gtk::HBox () );
@@ -2419,15 +2411,6 @@ bool FileCatalog::handleShortcutKey (GdkEventKey* event)
 
     if (!ctrl || (alt && !options.tabbedUI)) {
         switch(event->keyval) {
-
-        case GDK_KEY_bracketright:
-            coarsePanel->rotateRight();
-            return true;
-
-        case GDK_KEY_bracketleft:
-            coarsePanel->rotateLeft();
-            return true;
-
         case GDK_KEY_i:
         case GDK_KEY_I:
             exifInfo->set_active (!exifInfo->get_active());

rtgui/filecatalog.h
@@ -1,4 +1,5 @@
-/*
+/* -*- C++ -*-
+ *  
  *  This file is part of RawTherapee.
  *
  *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
@@ -150,10 +151,7 @@ public:
     // thumbnail browsers
     FileBrowser* fileBrowser;
 
-    CoarsePanel* coarsePanel;
-    ToolBar* toolBar;
-
-    FileCatalog (CoarsePanel* cp, ToolBar* tb, FilePanel* filepanel);
+    FileCatalog(FilePanel* filepanel);
     ~FileCatalog() override;
     void dirSelected (const Glib::ustring& dirname, const Glib::ustring& openfile);
     void closeDir    ();

rtgui/filepanel.cc
@@ -22,26 +22,6 @@
 #include "inspector.h"
 #include "placesbrowser.h"
 
-namespace {
-
-class DummyImageAreaToolListener: public ImageAreaToolListener {
-public:
-    DummyImageAreaToolListener()
-    {
-        tb_ = Gtk::manage(new ToolBar());
-    }
-    void spotWBselected(int x, int y, Thumbnail* thm = nullptr) {}
-    void sharpMaskSelected(bool sharpMask) {}
-    int getSpotWBRectSize() const { return 1; }
-    void cropSelectionReady() {}
-    void rotateSelectionReady(double rotate_deg, Thumbnail* thm = nullptr) {}
-    ToolBar* getToolBar() const { return tb_; }
-    CropGUIListener* startCropEditing(Thumbnail* thm = nullptr) { return nullptr; }
-private:
-    ToolBar *tb_;
-};
-
-} // namespace
 
 FilePanel::FilePanel () :
     parent(nullptr), error(0),
@@ -75,11 +55,8 @@ FilePanel::FilePanel () :
 
     dirpaned->pack1 (*placespaned, false, false);
 
-    //tpc = new BatchToolPanelCoordinator (this);
-    iatl_ = new DummyImageAreaToolListener();
     // Location bar
-    fileCatalog = Gtk::manage ( new FileCatalog (Gtk::manage(new CoarsePanel())/*tpc->coarse*/, iatl_->getToolBar()/*tpc->getToolBar()*/, this) );
-//    fileCatalog = Gtk::manage ( new FileCatalog (tpc->coarse, tpc->getToolBar(), this) );
+    fileCatalog = Gtk::manage(new FileCatalog(this));
     // Holds the location bar and thumbnails
     ribbonPane = Gtk::manage ( new Gtk::Paned() );
     ribbonPane->add(*fileCatalog);
@@ -121,9 +98,6 @@ FilePanel::FilePanel () :
     // sExportPanel->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
 
     fileCatalog->setFilterPanel (filterPanel);
-    // fileCatalog->setExportPanel (exportPanel);
-    //fileCatalog->setImageAreaToolListener (tpc);
-    fileCatalog->setImageAreaToolListener(iatl_);
 
     //------------------
 
@@ -184,9 +158,6 @@ FilePanel::~FilePanel ()
     if (inspectorPanel) {
         delete inspectorPanel;
     }
-
-//    delete tpc;
-    delete iatl_;
 }
 
 void FilePanel::on_realize ()

rtgui/filepanel.h
@@ -88,8 +88,6 @@ private:
 
     Inspector* inspectorPanel;
     Gtk::VPaned* tpcPaned;
-    //BatchToolPanelCoordinator* tpc;
-    ImageAreaToolListener *iatl_;
     History* history;
     RTWindow* parent;
     Gtk::Notebook* rightNotebook;

commit aad5e9fcbca63ab2417fb93600c239c6758c4909

fixed file browser, broken by dc5342345f15b39e249568e85da0f9c9ebc19ab3

rtgui/filebrowserentry.cc
@@ -42,7 +42,7 @@ Glib::RefPtr<Gdk::Pixbuf> FileBrowserEntry::hdr;
 Glib::RefPtr<Gdk::Pixbuf> FileBrowserEntry::ps;
 
 FileBrowserEntry::FileBrowserEntry (Thumbnail* thm, const Glib::ustring& fname)
-    : ThumbBrowserEntryBase (fname), wasInside(false), iatlistener(nullptr), press_x(0), press_y(0), action_x(0), action_y(0), rot_deg(0.0), landscape(true), cropgl(nullptr), state(SNormal), crop_custom_ratio(0.f)
+    : ThumbBrowserEntryBase (fname), wasInside(false), press_x(0), press_y(0), action_x(0), action_y(0), rot_deg(0.0), landscape(true), cropgl(nullptr), state(SNormal), crop_custom_ratio(0.f)
 {
     refresh_status_ = RefreshStatus::READY;
     thumbnail = thm;
@@ -320,9 +320,9 @@ bool FileBrowserEntry::motionNotify (int x, int y)
         }
     }
 
-    if (inside(x, y)) {
-        updateCursor(ix, iy);
-    }
+    // if (inside(x, y)) {
+    //     updateCursor(ix, iy);
+    // }
 
     if (state == SRotateSelecting) {
         action_x = x;
@@ -422,120 +422,6 @@ bool FileBrowserEntry::pressNotify   (int button, int type, int bstate, int x, i
 {
 
     bool b = ThumbBrowserEntryBase::pressNotify (button, type, bstate, x, y);
-
-    if (!iatlistener || !iatlistener->getToolBar()) {
-        return true;
-    }
-
-    ToolMode tm = iatlistener->getToolBar()->getTool ();
-    int ix = x - startx - ofsX;
-    int iy = y - starty - ofsY;
-
-    if (tm == TMNone) {
-        return b;
-    }
-
-    crop_custom_ratio = 0.f;
-
-    if (!b && selected && inside (x, y)) {
-        if (button == 1 && type == GDK_BUTTON_PRESS && state == SNormal) {
-            if ((bstate & GDK_SHIFT_MASK) && cropParams.w > 0 && cropParams.h > 0) {
-                crop_custom_ratio = float(cropParams.w) / float(cropParams.h);
-            }
-            if (onArea (CropTopLeft, ix, iy)) {
-                state = SResizeTL;
-                press_x = x;
-                action_x = cropParams.x;
-                press_y = y;
-                action_y = cropParams.y;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropTopRight, ix, iy)) {
-                state = SResizeTR;
-                press_x = x;
-                action_x = cropParams.w;
-                press_y = y;
-                action_y = cropParams.y;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropBottomLeft, ix, iy)) {
-                state = SResizeBL;
-                press_x = x;
-                action_x = cropParams.x;
-                press_y = y;
-                action_y = cropParams.h;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropBottomRight, ix, iy)) {
-                state = SResizeBR;
-                press_x = x;
-                action_x = cropParams.w;
-                press_y = y;
-                action_y = cropParams.h;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropTop, ix, iy)) {
-                state = SResizeH1;
-                press_y = y;
-                action_y = cropParams.y;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropBottom, ix, iy)) {
-                state = SResizeH2;
-                press_y = y;
-                action_y = cropParams.h;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropLeft, ix, iy)) {
-                state = SResizeW1;
-                press_x = x;
-                action_x = cropParams.x;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropRight, ix, iy)) {
-                state = SResizeW2;
-                press_x = x;
-                action_x = cropParams.w;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if ((bstate & GDK_SHIFT_MASK) && onArea (CropInside, ix, iy)) {
-                state = SCropMove;
-                press_x = x;
-                press_y = y;
-                action_x = cropParams.x;
-                action_y = cropParams.y;
-                cropgl = iatlistener->startCropEditing (thumbnail);
-                b = true;
-            } else if (onArea (CropImage, ix, iy)) {
-                if (tm == TMStraighten) {
-                    state = SRotateSelecting;
-                    press_x = x;
-                    press_y = y;
-                    action_x = x;
-                    action_y = y;
-                    rot_deg = 0;
-                    b = true;
-                } else if (tm == TMSpotWB) {
-                    iatlistener->spotWBselected ((ix - prex) / scale, (iy - prey) / scale, thumbnail);
-                    b = true;
-                } else if (tm == TMCropSelect) {
-                    cropgl = iatlistener->startCropEditing (thumbnail);
-
-                    if (cropgl) {
-                        state = SCropSelecting;
-                        press_x = cropParams.x = (ix - prex) / scale;
-                        press_y = cropParams.y = (iy - prey) / scale;
-                        cropParams.w = cropParams.h = 1;
-                        cropgl->cropInit (cropParams.x, cropParams.y, cropParams.w, cropParams.h);
-                        b = true;
-                    }
-                }
-            }
-        }
-
-        updateCursor (ix, iy);
-    }
-
     return b;
 }
 
@@ -543,36 +429,6 @@ bool FileBrowserEntry::releaseNotify (int button, int type, int bstate, int x, i
 {
 
     bool b = ThumbBrowserEntryBase::releaseNotify (button, type, bstate, x, y);
-
-    int ix = x - startx - ofsX;
-    int iy = y - starty - ofsY;
-
-    if (!b) {
-        if (state == SRotateSelecting) {
-            iatlistener->rotateSelectionReady (rot_deg, thumbnail);
-
-            if (iatlistener->getToolBar()) {
-                iatlistener->getToolBar()->setTool (TMHand);
-            }
-        } else if (cropgl && (state == SCropSelecting || state == SResizeH1 || state == SResizeH2 || state == SResizeW1 || state == SResizeW2 || state == SResizeTL || state == SResizeTR || state == SResizeBL || state == SResizeBR || state == SCropMove)) {
-            cropgl->cropManipReady ();
-            cropgl = nullptr;
-            iatlistener->cropSelectionReady ();
-
-            if (iatlistener->getToolBar()) {
-                iatlistener->getToolBar()->setTool (TMHand);
-            }
-        }
-
-        state = SNormal;
-
-        if (parent) {
-            parent->redrawNeeded (this);
-        }
-
-        updateCursor (ix, iy);
-    }
-
     return b;
 }
 
@@ -657,73 +513,6 @@ bool FileBrowserEntry::onArea (CursorArea a, int x, int y)
     }
 
     return false;
-}
-
-
-void FileBrowserEntry::updateCursor (int x, int y)
-{
-
-    if (!iatlistener || !iatlistener->getToolBar()) {
-        return;
-    }
-
-    CursorShape newCursor = CSUndefined;
-
-    ToolMode tm = iatlistener->getToolBar()->getTool ();
-    Glib::RefPtr<Gdk::Window> w = parent->getDrawingArea ()->get_window();
-
-    if (!selected) {
-        newCursor = CSArrow;
-    } else if (state == SNormal) {
-        if (tm == TMHand && (onArea (CropTop, x, y) || onArea (CropBottom, x, y))) {
-            newCursor = CSResizeHeight;
-        } else if (tm == TMHand && (onArea (CropLeft, x, y) || onArea (CropRight, x, y))) {
-            newCursor = CSResizeWidth;
-        } else if (tm == TMHand && (onArea (CropTopLeft, x, y))) {
-            newCursor = CSResizeTopLeft;
-        } else if (tm == TMHand && (onArea (CropTopRight, x, y))) {
-            newCursor = CSResizeTopRight;
-        } else if (tm == TMHand && (onArea (CropBottomLeft, x, y))) {
-            newCursor = CSResizeBottomLeft;
-        } else if (tm == TMHand && (onArea (CropBottomRight, x, y))) {
-            newCursor = CSResizeBottomRight;
-        } else if (onArea (CropImage, x, y)) {
-            if (tm == TMHand) {
-                newCursor = CSArrow;
-            } else if (tm == TMSpotWB) {
-                newCursor = CSSpotWB;
-            } else if (tm == TMCropSelect) {
-                newCursor = CSCropSelect;
-            } else if (tm == TMStraighten) {
-                newCursor = CSStraighten;
-            }
-        } else {
-            newCursor = CSArrow;
-        }
-    } else if (state == SCropSelecting) {
-        newCursor = CSCropSelect;
-    } else if (state == SRotateSelecting) {
-        newCursor = CSStraighten;
-    } else if (state == SCropMove) {
-        newCursor = CSMove;
-    } else if (state == SResizeW1 || state == SResizeW2) {
-        newCursor = CSResizeWidth;
-    } else if (state == SResizeH1 || state == SResizeH2) {
-        newCursor = CSResizeHeight;
-    } else if (state == SResizeTL) {
-        newCursor = CSResizeTopLeft;
-    } else if (state == SResizeTR) {
-        newCursor = CSResizeTopRight;
-    } else if (state == SResizeBL) {
-        newCursor = CSResizeBottomLeft;
-    } else if (state == SResizeBR) {
-        newCursor = CSResizeBottomRight;
-    }
-
-    if (newCursor != cursor_type) {
-        cursor_type = newCursor;
-        CursorManager::setCursorOfMainWindow (w, cursor_type);
-    }
 }
 
 

rtgui/filebrowserentry.h
@@ -52,7 +52,6 @@ class FileBrowserEntry : public ThumbBrowserEntryBase,
     double scale;
     static bool iconsLoaded;
     bool wasInside;
-    ImageAreaToolListener* iatlistener;
     int press_x, press_y, action_x, action_y;
     double rot_deg;
     bool landscape;
@@ -66,7 +65,7 @@ class FileBrowserEntry : public ThumbBrowserEntryBase,
     IdleRegister idle_register;
 
     bool onArea (CursorArea a, int x, int y);
-    void updateCursor (int x, int y);
+    // void updateCursor (int x, int y);
     void drawStraightenGuide (Cairo::RefPtr<Cairo::Context> c);
     void customBackBufferUpdate (Cairo::RefPtr<Cairo::Context> c) override;
 
@@ -85,11 +84,6 @@ public:
     FileBrowserEntry (Thumbnail* thm, const Glib::ustring& fname);
     ~FileBrowserEntry () override;
     void draw (Cairo::RefPtr<Cairo::Context> cc) override;
-
-    void setImageAreaToolListener (ImageAreaToolListener* l)
-    {
-        iatlistener = l;
-    }
 
     FileThumbnailButtonSet* getThumbButtonSet ();
 

rtgui/filecatalog.cc
@@ -48,7 +48,6 @@ FileCatalog::FileCatalog(FilePanel* filepanel) :
     actionNextPrevious(NAV_NONE),
     listener(nullptr),
     fslistener(nullptr),
-    iatlistener(nullptr),
     hbToolBar1STB(nullptr),
     hasValidCurrentEFS(false),
     filterPanel(nullptr),
@@ -735,7 +734,6 @@ void FileCatalog::previewReady (int dir_id, FileBrowserEntry* fdn)
     }
 
     // put it into the "full directory" browser
-    fdn->setImageAreaToolListener (iatlistener);
     fileBrowser->addEntry (fdn);
 
     // update exif filter settings (minimal & maximal values of exif tags, cameras, lenses, etc...)

rtgui/filecatalog.h
@@ -63,7 +63,6 @@ private:
 
     FileSelectionListener* listener;
     FileSelectionChangeListener* fslistener;
-    ImageAreaToolListener* iatlistener;
     DirSelectionSlot selectDir;
 
     Gtk::HBox* buttonBar;
@@ -220,10 +219,6 @@ public:
     void setFileSelectionChangeListener (FileSelectionChangeListener* l)
     {
         fslistener = l;
-    }
-    void setImageAreaToolListener (ImageAreaToolListener* l)
-    {
-        iatlistener = l;
     }
     void setDirSelector (const DirSelectionSlot& selectDir);
 